4cfe584b832efe963519d8a630d9487141894522
Open Source SW <opensw2024@gmail.com>
2024-05-10 15:18:44 +0900

    New lone about log

--- newfeature.c
+++ newfeature.c
@@ -4,7 +4,10 @@
 void main()
 {
     char pwd[1024];
+    FILE *f = fopen("pwd-log.log", 'w');
     getcwd(pwd, sizeof(pwd));
-    printf("%s\n",pwd);
+    printf("print working dir: %s\n",pwd);
+    fputs("%s\n",f);
+    fclose(f);
 }
 
